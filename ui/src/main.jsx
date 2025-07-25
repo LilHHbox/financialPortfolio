701b6a24dc89309a83efd9c5ed778978a58bd9c5
hAoZeZzz <haoze883@gmail.com>
2025-07-25 15:45:26 +0800

    Adjust the Pie Chart && Create route

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Dashboard from './components/Dashboard.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Dashboard />
+    <Router>
+      <Routes>
+        {/* Map the '/' route to the Dashboard component */}
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
